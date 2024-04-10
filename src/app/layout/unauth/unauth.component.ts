5a5aaba2de5f4bd173ffa52cad5ceadf7b8eb0b3
Onwordi Prince Alfred <princelini4@gmail.com>
2024-04-10 16:13:46 +0100

    layout added

--- src/app/layout/unauth/unauth.component.ts
+++ src/app/layout/unauth/unauth.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-unauth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './unauth.component.html',
-  styleUrls: ['./unauth.component.scss']
+  styleUrls: ['./unauth.component.scss'],
 })
-export class UnauthComponent {
-
-}
+export class UnauthComponent {}
