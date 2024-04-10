f5a0818e0615bbc64d39388ad272dcb809283a61
Onwordi Prince Alfred <princelini4@gmail.com>
2024-04-10 19:56:35 +0100

    updates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from 'src/app/components/home/hero/hero.component';
+import { LoanComponent } from 'src/app/components/home/loan/loan.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeroComponent],
+  imports: [CommonModule, HeroComponent, LoanComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
